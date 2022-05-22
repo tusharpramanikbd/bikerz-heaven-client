78d0fe2a59d0ca1d86507d1cae71a760a176da85
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-05-22 22:30:41 +0600

    business summary section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './Banner'
 import BikeParts from './BikeParts'
+import BusinessSummary from './BusinessSummary'
 
 const Home = () => {
   return (
     <div className='bg-gray-100 pb-4'>
       <Banner />
       <BikeParts />
+      <BusinessSummary />
     </div>
   )
 }
