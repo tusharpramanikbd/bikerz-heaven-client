7a4a9df45154298772296f06267855c213ec6887
Tushar Pramanik <tusharpramanikbd@gmail.com>
2022-05-24 01:26:58 +0600

    basic place order mechanism added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import RequireAuth from './Pages/Login/RequireAuth'
 import SignUp from './Pages/Login/SignUp'
 import Purchase from './Pages/Purchase/Purchase'
 import Navbar from './Pages/Shared/Navbar'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   return (
@@ -27,6 +29,7 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
       </Routes>
+      <ToastContainer />
     </div>
   )
 }
--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -1,19 +1,37 @@
 import axios from 'axios'
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import Loading from '../Shared/Loading'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import auth from '../../firebase.init'
 import TitleUnderline from '../Shared/TitleUnderline'
+import { useForm } from 'react-hook-form'
+import { toast } from 'react-toastify'
+import { format } from 'date-fns'
 
 const Purchase = () => {
   const [user] = useAuthState(auth)
   const { id } = useParams()
+  const [quantity, setQuantity] = useState(0)
+  const [quantityError, setQuantityError] = useState('')
+  const [isDisabled, setIsDisabled] = useState(false)
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm()
 
   const { data: bikePart, isLoading } = useQuery(
     ['bikePart', id],
-    async () => await axios.get(`http://localhost:5000/bikeParts/${id}`)
+    async () => await axios.get(`http://localhost:5000/bikeParts/${id}`),
+    {
+      onSuccess: (data) => {
+        setQuantity(data.data.minOrderQuantity)
+      },
+    }
   )
 
   if (isLoading) {
@@ -28,6 +46,39 @@ const Purchase = () => {
     availableQuantity,
     price,
   } = bikePart.data
+
+  const handleQuantityChange = (event) => {
+    setQuantity(event.target.value)
+    if (event.target.value === '') {
+      setQuantityError('You have to add minimum order quantity')
+      setIsDisabled(true)
+    } else if (parseInt(event.target.value) < minOrderQuantity) {
+      setQuantityError('You can not order less than minimum order quantity')
+      setIsDisabled(true)
+    } else if (parseInt(event.target.value) > availableQuantity) {
+      setQuantityError('You can not order more than available quantity')
+      setIsDisabled(true)
+    } else {
+      setQuantityError('')
+      setIsDisabled(false)
+    }
+  }
+
+  const onSubmit = async (data) => {
+    data.payment = 'unpaid'
+    const orderDate = format(new Date(), 'PP')
+    data.orderDate = orderDate
+    data.productId = id
+    axios
+      .post('http://localhost:5000/orders', {
+        data,
+      })
+      .then((res) => {
+        reset()
+        toast.success('Your order is recieved')
+      })
+      .catch((err) => console.log(err))
+  }
 
   return (
     <div className='bg-gray-100 p-4 min-h-[calc(100vh-64px)]'>
@@ -56,6 +107,112 @@ const Purchase = () => {
             </h2>
           </div>
         </div>
+
+        <h2 className='text-center text-2xl font-bold mt-4'>Place Order</h2>
+        <TitleUnderline />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text'>Name</span>
+            </label>
+            <input
+              type='text'
+              readOnly
+              value={user.displayName}
+              className='input input-bordered w-full'
+              {...register('name')}
+            />
+          </div>
+
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text'>Email</span>
+            </label>
+            <input
+              type='email'
+              readOnly
+              value={user.email}
+              className='input input-bordered w-full'
+              {...register('email')}
+            />
+          </div>
+
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text'>Shipping Address</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Shipping Address'
+              className='input input-bordered w-full'
+              {...register('address', {
+                required: {
+                  value: true,
+                  message: 'Shipping address is required',
+                },
+              })}
+            />
+            <label className='label'>
+              {errors.address?.type === 'required' && (
+                <span className='label-text-alt text-red-500'>
+                  {errors.address.message}
+                </span>
+              )}
+            </label>
+          </div>
+
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text'>Phone Number</span>
+            </label>
+            <input
+              type='text'
+              placeholder='Phone Number'
+              className='input input-bordered w-full'
+              {...register('phone', {
+                required: {
+                  value: true,
+                  message: 'Phone number is required',
+                },
+              })}
+            />
+            <label className='label'>
+              {errors.phone?.type === 'required' && (
+                <span className='label-text-alt text-red-500'>
+                  {errors.phone.message}
+                </span>
+              )}
+            </label>
+          </div>
+
+          <div className='form-control w-full'>
+            <label className='label'>
+              <span className='label-text'>Order Quantity</span>
+            </label>
+            <input
+              type='number'
+              value={quantity}
+              className='input input-bordered w-full'
+              {...register('orderQuantity', {
+                onChange: (e) => {
+                  handleQuantityChange(e)
+                },
+              })}
+            />
+            <label className='label'>
+              <span className='label-text-alt text-red-500'>
+                {quantityError}
+              </span>
+            </label>
+          </div>
+
+          <input
+            className='btn w-full text-white'
+            type='submit'
+            value='Purchase'
+            disabled={isDisabled}
+          />
+        </form>
       </div>
     </div>
   )
